e8b6808a35c75376c432473bcc287c8210b4d48f
Scott Sutton <sssutton10@gmail.com>
2022-03-01 20:33:47 -0500

    added backspace, equals functionality. Fixed calculation bugs

--- javascript.js
+++ javascript.js
@@ -1,13 +1,18 @@
+'use-strict';
+
 // Global variables
 let equation = document.querySelector('.equation')
 let tempResult = document.querySelector('.temp-result')
-let eqStr = '' // Store the operators for equation evaluation
+let tempNum = '' // Storing the number being typed to allow for multi-digit numbers
+let ops = [] // Push operators and numbers to respective arrays as typed, pop when deleting
+let nums = []
+let priorOffsetHeight = 0 // For checking if text wraps - if so then half size
 
 function add(a, b){
     return a+b
 }
 
-function substract(a, b){
+function subtract(a, b){
     return a-b
 }
 
@@ -23,8 +28,8 @@ function operate(sign, num1, num2){
     switch (sign){
         case '+':
             return add(num1, num2)
-        case '-':
-            return substract(num1, num2)
+        case '−':
+            return subtract(num1, num2)
         case '*':
             return multiply(num1, num2)
         default:
@@ -40,10 +45,104 @@ function keyPress(e){
     valButton.dispatchEvent(event)
 }
 
-function numberPress(e){
+function buttonPress(e){
     equation.textContent += this.textContent
+    fontSizeAdjust(equation)
+    tempResult.textContent = numOperatorAction(this.classList[0], this.textContent)
 }
 
+function evalEquation(opList, numList){
+    let retNum
+    for(let i=0; i < opList.length; i++){
+        if((i == opList.length - 1) && (ops.length != nums.length+1)){
+            retNum = operate(opList[i], retNum || numList[i], parseInt(tempNum))
+        }
+        else{
+            retNum = operate(opList[i], retNum || numList[i], numList[i+1])
+        }
+    }
+
+    return retNum
+}
+
+function numOperatorAction(bType, char){
+    if (bType == 'operator'){
+        tempResult.textContent = ''
+        ops.push(char)
+        nums.push(parseInt(tempNum))
+        tempNum = ''
+        return ''
+    }
+    else if (bType == 'number'){
+        tempNum += char
+        return evalEquation(ops, nums)
+    }
+}
+
+function equalsPress(e){
+    e.preventDefault()
+    if(isNaN(equation.textContent.slice(-1))){return} // disable button if last entered button is not a number
+    equation.textContent = `${tempResult.textContent}`
+    tempResult.textContent = ''
+    ops = []
+    nums = []
+    tempNum = equation.textContent
+    equation.style.fontSize = '2rem'
+}
+
+function backspacePress(e){
+    equation.textContent = equation.textContent.slice(0, equation.textContent.length-1)
+    remChar = tempNum.slice(-1)
+    tempNum = tempNum.slice(0, tempNum.length - 1)
+    if(isNaN(parseInt(remChar))){
+        ops.pop()
+        tempResult.textContent = evalEquation(ops, nums)
+    }
+    else if(isNaN(parseInt(tempNum.slice(-1)))){
+        tempResult.textContent = ''
+        nums = equation.textContent.split(/[+-\/\*]/).filter(x => x).map(x => parseInt(x))
+    }
+    else{
+        tempResult.textContent = evalEquation(ops, nums)
+    }
+}
+
+// Button event listeners for basic calculating
 document.addEventListener('keyup', keyPress)
+
 let buttons = document.querySelectorAll('.number')
-buttons.forEach(element => element.addEventListener('click', numberPress))
+buttons.forEach(element => element.addEventListener('click', buttonPress))
+
+let operators = document.querySelectorAll('.operator')
+operators.forEach(element => element.addEventListener('click', buttonPress))
+
+document.querySelector('.equals').addEventListener('click', equalsPress)
+document.querySelector('.backspace').addEventListener('click', backspacePress)
+
+// Clear button
+function clearScreen(){
+    equation.textContent = ''
+    tempResult.textContent = ''
+    priorOffsetHeight = 0
+    ops = []
+    nums = []
+    tempNum = ''
+    equation.style.fontSize = '2rem'
+}
+
+document.querySelector('.clear').addEventListener('click', clearScreen)
+
+// Font size function for equation div - if it tries to wrap then decrease the font size
+function fontSizeAdjust(div){
+    if((div.offsetHeight > priorOffsetHeight) && (priorOffsetHeight > 0)){
+        currSize = parseInt(getComputedStyle(div)['font-size'].slice(0, -2))
+        newSize = currSize / 2 + 'px'
+        div.style.fontSize = newSize
+    }
+    else if((div.offsetHeight < priorOffsetHeight) && (div.offsetHeight > 0)){
+        currSize = parseInt(getComputedStyle(div)['font-size'].slice(0, -2))
+        newSize = currSize * 2 + 'px'
+        div.style.fontSize = newSize
+    }
+    priorOffsetHeight = div.offsetHeight
+}
